63e95abd73fcb8adfd02cbf20aa2e427c83445c7
Jose Ortiz Costa <joseortizcostadev@gmail.com>
2020-05-16 19:31:52 -0700

    Updated Peer

--- Projects/P2P-Decentralized-Network/peer.py
+++ Projects/P2P-Decentralized-Network/peer.py
@@ -1,7 +1,16 @@
 from client import Client
 from server import Server
 
-class Peer(Client, Server):
+class Peer():
+    
+    SEEDER = 2;
+    LEECHER = 1;
+    PEER = 0;
+    
 
     def __init__(self):
+        self.role = PEER # default role
         pass
+    
+    # your peer inplementation here. 
+    # You may rehuse the code from your labs here as help for your final implementation of this class.
