de0f666bd1c2dd60ea87b72e5c3292e8cb9a5d62
sfsu <jortizco@sfsu.edu>
2020-05-26 15:11:46 -0700

    Refactored client_handler.py

--- labs/lab4/client_handler.py
+++ labs/lab4/client_handler.py
@@ -11,41 +11,47 @@
 # Running instructions: This program needs the server to run. The server creates an object of this class.
 #
 ########################################################################################################################
-import threading
 
-class ClientHandler(object):
+import threading
+import pickle
+class ClientHandler:
     """
     The client handler class receives and process client requests
     and sends responses back to the client linked to this handler.
     """
-    def __init__(self, server_instance, clienthandler, addr):
+    def __init__(self, server_instance, clientsocket, addr):
         """
         Class constructor already implemented for you.
-        :param server_instance:
-        :param clienthandler:
-        :param addr:
+        :param server_instance: passed as 'self' when the object of this class is created in the server object
+        :param clientsocket: the accepted client on server side
+        :param addr: addr[0] = server ip address, addr[1] = client id assigned buy the server
         """
-        self.clientid = addr[1] # the id of the client that owns this handler
         self.server_ip = addr[0]
+        self.client_id = addr[1]
         self.server = server_instance
-        self.clienthandler = clienthandler
-
-    def print_lock(self):
-        """
-        TODO: create a new print lock
-        :return: the lock created
-        """
-        # your code here.
-        return None # modify the return to return a the lock created
+        self.handler = clientsocket
+        self.print_lock = threading.Lock() # creates the print lock
 
     def process_client_data(self):
         """
         TODO: receives the data from the client
         TODO: prepares the data to be printed in console
-        TODO: create a print lock
-        TODO: adquire the print lock
+        TODO: acquire the print lock
         TODO: prints the data in server console
         TODO: release the print lock
+        TODO: keep this handler object listening for more incoming data from the client
         :return: VOID
         """
         pass # remove this line after implemented.
+
+    def send(self, data):
+        serialized_data = pickle.dumps(data)
+        self.handler.send(data)
+
+    def receive(self, max_mem_alloc=4096):
+        raw_data = self.handler.recv(max_mem_alloc)
+        data = pickle.loads(raw_data)
+        return data
+
+    def run(self):
+        self.process_client_data()
